717097ff7f7fd1a6b24c2a40d97b37f964b05881
Laura Santos <alsr16p@gmail.com>
2021-10-12 17:14:37 +0100

    start the template

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  let today = new Date();
+
+  let currentDay = today.getDay();
+
+  //0 -> Sunday; 1 to 5 -> Monday until Friday, 6 -> Saturday
+  if (currentDay === 6 || currentDay === 0) {
+      res.send("Yay! It's weekend!");
+  } else {
+      res.sendFile(__dirname + "/index.html");
+  }
+
 });
 
 app.listen(port, () => {

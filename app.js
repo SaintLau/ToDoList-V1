8ea504f2437f5b675ccf009bf6e10d5e43ed0c7e
Laura Santos <alsr16p@gmail.com>
2021-10-13 14:58:58 +0100

    button to add new task. Set server so save new value

--- app.js
+++ app.js
@@ -7,43 +7,35 @@ const bodyParser = require('body-parser');
 //for ejs:
 app.set('view engine', 'ejs');
 
+//for body parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+let items = []; //need to declare at the top and reset later the value otherwise app will crash due to scope
+
 app.get('/', (req, res) => {
-  let today = new Date();
+  
+ let today = new Date();
 
-  let currentDay = today.getDay();
-  let day = "";
+  let options = {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long'
+  };
 
+  let day = today.toLocaleDateString("en-US", options);
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-
-        default:
-            console.log("Error: current day is equal to: " + currentDay); //-> to see what cause the error 
-}
   res.render("list", {
-      kindOfDay: day
-    }); //-> list is the doc that needs to exict inside the views. kindOfDay is the key in the list doc to where we want to send data. The data we want to send is inside the day var. In these case we can render at the end of the if statement 
+    kindOfDay: day,
+    newListItems: items
+  }); //-> list is the doc that needs to exict inside the views. kindOfDay is the key in the list doc to where we want to send data. The data we want to send is inside the day var. Second pair refer to add new items to the list  
+});
 
+app.post('/', (req, res) => {
+    let item = req.body.newItem;
+
+    items.push(item);
+    
+    res.redirect("/");
 });
 
 app.listen(port, () => {
